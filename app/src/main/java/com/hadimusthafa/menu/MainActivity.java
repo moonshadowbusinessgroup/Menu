ec261b150d18466d34b742ad6c72657cefca4e8c
moon <moonshadowbusinessgroup@gmail.com>
2021-10-17 20:41:40 +0530

    ok

---
+++ app/src/main/java/com/hadimusthafa/menu/MainActivity.java
@@ -0,0 +1,32 @@
+package com.hadimusthafa.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_notes_24);
+//      toolbar.setLogo(R.drawable.ic_baseline_notes_24);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
